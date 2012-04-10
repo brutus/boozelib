e1d49acea17e9f060e71deffd5436c2048ec5033
Brutus [dmc] <brutus.dmc@googlemail.com>
2012-04-10 15:27:24 +0200

    added test for repr, str, as_list

--- test/test_boozelib.py
+++ test/test_boozelib.py
@@ -3,13 +3,15 @@ from nose.tools import *
 from boozelib import *
 
 
+# TEST DATA
+
 f1 = User(32, 42, 155, True)
 f2 = User(30, 80, 173, True)
 m1 = User(30, 68, 172, False)
-m2 = User(34, 103, 186, False)
+m2 = User(34, 103, 186, False, 'ThatGuy')
 
 beer = Drink(500, 4.9)
-shot = Drink(100, 40)
+shot = Drink(100, 40, 'Wodka')
 
 test_cases_bac = (
   (f1, beer, 0.597136937838),
@@ -25,6 +27,29 @@ test_cases_degradation = (
   (m2, 60, 0.217695813527036),
 )
 
+test_cases_repr = (
+  (f1, "<User(32, 42, 155, True, None)>"),
+  (m2, "<User(34, 103, 186, False, 'ThatGuy')>"),
+  (beer, "<Drink(500, 4.9, None)>"),
+  (shot, "<Drink(100, 40.0, 'Wodka')>")
+)
+
+test_cases_str = (
+  (f1, "[None] 32 years, 42 kg, 155 cm (female): 0"),
+  (m2, "[ThatGuy] 34 years, 103 kg, 186 cm (male): 0"),
+  (beer, "[None] Volumen: 500, Percent: 4.9"),
+  (shot, "[Wodka] Volumen: 100, Percent: 40.0")
+)
+
+test_cases_list = (
+  (f1, [32, 42, 155, True, None]),
+  (m2, [34, 103, 186, False, 'ThatGuy']),
+  (beer, [500, 4.9, None]),
+  (shot, [100, 40.0, 'Wodka'])
+)
+
+
+# HELPER FUNCTIONS
 
 def get_bac_(user, drink):
   return get_bac(
@@ -56,6 +81,17 @@ def check_degrade_bac(user, bac):
   b2 = degrade_bac_(user, bac, 60)
   assert_almost_equal(b1, b2)
 
+def check_repr(instance, expected):
+  assert_equal(instance.__repr__(), expected)
+
+def check_str(instance, expected):
+  assert_equal(instance.__str__(), expected)
+
+def check_as_list(instance, expected):
+  assert_equal(instance.as_list(), expected)
+
+
+# TESTS
 
 def test_get_bac():
   for user, drink, expected in test_cases_bac:
@@ -102,3 +138,16 @@ def test_User():
   bac += get_bac_(guy, beer)
   bac -= get_degradation_(guy, 10)
   assert_almost_equal(guy.bac, bac)
+
+def test_repr():
+  for ins, expected in test_cases_repr:
+    yield check_repr, ins, expected
+
+def test_str():
+  for ins, expected in test_cases_str:
+    yield check_str, ins, expected
+
+def test_as_list():
+  for ins, expected in test_cases_list:
+    yield check_as_list, ins, expected
+
