764d4e0ff48904cae2bbbd25a505a56c053f3dd4
Janto <janto@team.uber.space>
2021-03-08 20:05:15 +0100

    :wrench: tweak mypy config

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import nox
+from nox.sessions import Session
 
 
 PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
@@ -16,7 +17,7 @@ nox.options.reuse_existing_virtualenvs = True
 
 
 @nox.session(python=PYTHON_VERSIONS)
-def test(session):
+def test(session: Session) -> None:
     """ Run doctests and ward test suit. """
     session.install(*TOOLS_TEST)
     session.run("python", "-m", "doctest", *(str(p) for p in PYTHON_FILES_SOURCE))
